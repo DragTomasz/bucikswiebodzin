e8e131223624e4beb5b1f6e8c58ecec8a96211de
Tomasz <drag.tomasz@gmail.com>
2024-06-21 23:11:14 +0200

    map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
 import {ProductWrapperComponent} from "./product-wrapper/product-wrapper.component";
+import {ContactComponent} from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProductWrapperComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, ProductWrapperComponent, FooterComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenubarModule} from "primeng/menubar";
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  ngOnInit() {
+
+  }
 }
